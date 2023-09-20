f8e295d973a50a5a80913cf1c667633c0420e4a7
Ahmad Fadilah <ahmdfadilah7@gmail.com>
2023-09-20 22:10:35 +0700

    TEMPLATE

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    // Menampilkan halaman login
+    public function login()
+    {
+        return view('auth.login');
+    }
+
+    // Menampilkan halaman register
+    public function register()
+    {
+        return view('auth.register');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('login', [AuthController::class, 'login'])->name('login');
+Route::get('/', [AuthController::class, 'login'])->name('login');
 Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
